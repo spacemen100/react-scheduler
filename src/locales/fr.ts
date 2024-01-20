5d539828ad83c8d61dfdae665ef48b78f7feefbd
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-01-20 16:03:09 +0100

    Create fr.ts

---
+++ src/locales/fr.ts
@@ -0,0 +1,18 @@
+export const fr = {
+    feelingEmpty: "Je me sens si vide...",
+    free: "Libre",
+    loadNext: "Suivant",
+    loadPrevious: "Précédent",
+    over: "fini",
+    taken: "Pris",
+    topbar: {
+      filters: "Filtres",
+      next: "suivant",
+      prev: "précédent",
+      today: "Aujourd'hui",
+      view: "Vue"
+    },
+    search: "recherche",
+    week: "semaine"
+  };
+  
